a3853325840e093572738346c2287a8334fc6202
Satvik Pisipati <satpis@az.zli.ch>
2021-07-08 16:00:26 +0200

    Initial commit

---
+++ app/src/main/java/ch/zli/july/InfoPage.java
@@ -0,0 +1,14 @@
+package ch.zli.july;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_page);
+    }
+}
---
+++ app/src/main/java/ch/zli/july/MainActivity.java
@@ -0,0 +1,57 @@
+package ch.zli.july;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+
+    public void play(){
+        mediaPlayer.start();
+    }
+
+    public void pause(){
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        Button help = findViewById(R.id.help);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
+        help.setOnClickListener(v -> {
+            if(help.getText().equals("HELP")){
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
+                play();
+                help.setText("STOP");
+
+            } else {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+                pause();
+                help.setText("HELP");
+            }
+        });
+
+        Button info = (Button) findViewById(R.id.info);
+        info.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), InfoPage.class);
+                startActivity(myIntent);
+            }
+         });
+    }
+}
